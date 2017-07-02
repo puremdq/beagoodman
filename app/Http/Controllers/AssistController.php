be0dfb2b7fdfc4b3a4d066918c9b79020a0e52e6
Pure <puremdq@gmail.com>
2017-07-03 00:15:02 +0800

    再次更新

--- app/Http/Controllers/AssistController.php
+++ app/Http/Controllers/AssistController.php
@@ -2,82 +2,76 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Auth\Access\Response;
-use Illuminate\Http\Request;
 use App\Http\Model\File;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Http\Request;
 
 class AssistController extends Controller
 {
 
 
-    public function getFile()
+    public function getFile(Request $request)
     {
+        $key = $request->input('key', '');
 
-
-        $key = $_GET['key'];
         $type = 'jpeg';
 
-        try {
-
-            $img = Redis::get($key);
-
-
-        } catch (\Exception $exception) {
-
+        if (!Redis::exists($key)) {
 
             try {
 
-                $img = (Redis::lrange($key, 0, -1))[0];
-                $name = (Redis::lrange($key . "_name", 0, -1))[0];
+                $img = Redis::get($key);
 
-                if (!empty($name)) {
-                    $extension = substr(strrchr($name, '.'), 1);
-                    if (in_array($extension, array('jpg', 'gif', 'png', 'jpeg'))) {
-                        $type = $extension;
-                    }
-
-                }
-
-            } catch (\Exception $e) {
-
-                //echo $exception->getMessage();
-                abort(404);
-            }
-
-        } finally {
-
-            if (!empty($img)) {
-                header('Content-type: image/' . $type);
-                echo $img;
-            } else {
+            } catch (\Exception $exception) {
 
                 try {
-                    $url = '/';
-                    $redisKey = $key . '_url';
 
-                    if (!Redis::exists($redisKey)) {
+                    $img = (Redis::lrange($key, 0, -1))[0];
+                    $name = (Redis::lrange($key . "_name", 0, -1))[0];
 
-                        $url = Redis::get($redisKey);
-
-                    } else {
-
-                        $file = File::where('file_key', $key)->firstOrFail();
-                        $url = $file->file_url;
-                        Redis::set($redisKey,$url);
-                        Redis::expire($redisKey, 3600);//设置过期时间
+                    if (!empty($name)) {
+                        $extension = substr(strrchr($name, '.'), 1);
+                        if (in_array($extension, array('jpg', 'gif', 'png', 'jpeg'))) {
+                            $type = $extension;
+                        }
                     }
 
-                    return redirect(url($url));
+                } catch (\Exception $e) {
 
-                } catch (\Exception $findException) {
-
-                    abort(404);
+                    //echo $exception->getMessage();
+                    return abort(404);
                 }
 
+            }
+            header('Content-type: image/' . $type);
+            echo $img;
+        } else {
+
+            try {
+                $url = '/';
+                $redisKey = $key . '_url';
+
+                if (!Redis::exists($redisKey)) {
+
+                    $url = Redis::get($redisKey);
+
+                } else {
+
+                    $file = File::where('file_key', $key)->firstOrFail();
+                    $url = $file->file_url;
+                    Redis::set($redisKey, $url);
+                    Redis::expire($redisKey, 3600);//设置过期时间
+                }
+
+                return redirect(url($url));
+
+            } catch (\Exception $findException) {
+
+                return abort(404);
             }
 
         }
+
+
     }
 }
